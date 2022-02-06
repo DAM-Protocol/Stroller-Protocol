36630ff43f5271181edbfe583c98b0f0c1fcc931
Pushkar Borkar <pushkarborkar1809@gmail.com>
2022-02-07 03:29:32 +0530

    Favicon and metadata updated

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,15 +1,19 @@
-import { Text, Heading } from '@chakra-ui/react';
+import { Text, Heading, Flex } from '@chakra-ui/react';
 import Page from '../components/layouts/Page';
 
 const Landing = () => {
 	return (
 		<Page>
-			<Heading as='h1' size='2xl'>
-				Stroll Protocol
-			</Heading>
-			<Text>
-				Stroll is a protocol that provides automatic Top-Ups for Super Tokens
-			</Text>
+			<Flex flexDirection='column' align='center' justify='center'>
+				<img src='./stroller-xxl.png' alt='X' style={{ width: '60px' }} />
+				<Heading as='h1' size='2xl'>
+					Stroller Protocol
+				</Heading>
+				<Text>Unlock the potential of SuperTokens</Text>
+				<Text>
+					Stroll is a protocol that provides automatic Top-Ups for Super Tokens
+				</Text>
+			</Flex>
 		</Page>
 	);
 };
