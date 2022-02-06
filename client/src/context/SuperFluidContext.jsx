6958ab7a22164083a1aa412036b08b54dcb1d15e
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-02-06 17:23:47 +0530

    Fetch user supertokens

--- client/src/context/SuperFluidContext.jsx
+++ client/src/context/SuperFluidContext.jsx
@@ -1,6 +1,6 @@
 import { Framework, Query } from '@superfluid-finance/sdk-core';
 import { createContext, useState, useMemo } from 'react';
-import { useMoralis } from 'react-moralis';
+import { useMoralis, useMoralisWeb3Api } from 'react-moralis';
 import { ethers } from 'ethers';
 import { useEffect } from 'react';
 import useDefaultERC20Tokens from '../hooks/useDefaultERC20Tokens';
@@ -12,54 +12,119 @@ const SuperFluidContext = createContext({ sf: null });
 const SuperFluidProvider = ({ children }) => {
 	const { web3, isWeb3Enabled } = useMoralis();
 
-	console.log('isWeb3Enabled', isWeb3Enabled);
-
 	const [sf, setSf] = useState(null);
+	useEffect(() => {
+		(async () => {
+			if (!isWeb3Enabled) return null;
 
-	// useEffect(() => {
-	// 	(async () => {
-	// 		if (!isWeb3Enabled) return null;
+			const mmProvider = new ethers.providers.Web3Provider(window.ethereum);
 
-	// 		const mmProvider = new ethers.providers.Web3Provider(window.ethereum);
-
-	// 		const _sf = await Framework.create({
-	// 			networkName: 'matic',
-	// 			provider: mmProvider,
-	// 		});
-	// 		window.sf = _sf;
-	// 		setSf(_sf);
-	// 	})();
-	// }, [isWeb3Enabled, web3]);
+			const _sf = await Framework.create({
+				networkName: 'matic',
+				provider: mmProvider,
+			});
+			window.sf = _sf;
+			setSf(_sf);
+		})();
+	}, [isWeb3Enabled, web3]);
 
 	const { tokensLookup, tokenAddresses } = useDefaultERC20Tokens();
 	const [defaultTokenList, setDefaultTokenList] = useState([]);
+	const [userTokenList, setUserTokenList] = useState([]);
+	window.defaultTokenList = defaultTokenList;
 
-	const [getSuperTokenList, { data: defaultSuperTokenData }] =
-		useSfSubgraphLazyQuery(GET_SUPER_TOKENS);
+	const [getSuperTokenList] = useSfSubgraphLazyQuery(GET_SUPER_TOKENS);
 
 	useEffect(() => {
-		if (tokenAddresses)
-			getSuperTokenList({
-				variables: { where: { underlyingAddress_in: tokenAddresses } },
+		(async () => {
+			const { data: defaultSuperTokenData } = await getSuperTokenList({
+				variables: {
+					where: {
+						underlyingAddress_in: tokenAddresses,
+						underlyingAddress_not: '0x',
+					},
+				},
 			});
-	}, [getSuperTokenList, tokenAddresses]);
+			if (defaultSuperTokenData?.tokens) {
+				const tempTokenList = [];
 
-	useEffect(() => {
-		if (defaultSuperTokenData?.tokens) {
-			const tempTokenList = [];
-			defaultSuperTokenData.tokens.forEach((token) => {
-				tempTokenList.push({
-					...token,
-					tk: tokensLookup[token?.underlyingAddress],
+				defaultSuperTokenData.tokens.forEach((token) => {
+					tempTokenList.push({
+						...token,
+						tk: {
+							...tokensLookup[token?.underlyingAddress],
+							address: token?.underlyingAddress,
+						},
+					});
 				});
-			});
-			console.log(tempTokenList);
-			setDefaultTokenList(tempTokenList);
-		}
-	}, [defaultSuperTokenData]);
+				setDefaultTokenList(tempTokenList);
+			}
+		})();
+	}, [getSuperTokenList, tokenAddresses, tokensLookup]);
+
+	const [getUserSuperTokenList] = useSfSubgraphLazyQuery(GET_SUPER_TOKENS);
+	const Web3Api = useMoralisWeb3Api();
+
+	useEffect(() => {
+		(async () => {
+			if (sf) {
+				let pageResult = await sf.query?.listUserInteractedSuperTokens({
+					account: '0x452181dAe31Cf9f42189df71eC64298993BEe6d3'.toLowerCase(),
+				});
+				if (pageResult.data) {
+					const data = pageResult.data;
+					const superTokens = data.map((sfToken) => sfToken.token.id);
+					// get underlying tokens to fetch metadata from moralis
+					const underlyingTokens = data.map(
+						(sfToken) => sfToken.token.underlyingAddress
+					);
+
+					const tokenMetadata = await Web3Api.token.getTokenMetadata({
+						chain: 'matic',
+						addresses: underlyingTokens,
+					});
+					// create a lookup table for token metadata
+					const tokenMetadataLookup = {};
+					tokenMetadata.forEach(({ decimals, name, symbol, address }) => {
+						tokenMetadataLookup[address] = {
+							decimals,
+							name,
+							symbol,
+							address,
+						};
+					});
+
+					// get supertoken data from the graph
+					const { data: userTokenData } = await getUserSuperTokenList({
+						variables: {
+							where: {
+								id_in: superTokens,
+							},
+						},
+					});
+
+					const tempTokenList = [];
+
+					// add the user supertokens to the list
+					const _userTokens = userTokenData.tokens;
+					_userTokens.forEach((token) => {
+						tempTokenList.push({
+							...token,
+							tk: {
+								...tokenMetadataLookup[token?.underlyingAddress],
+								address: token?.underlyingAddress,
+							},
+						});
+					});
+					setUserTokenList(tempTokenList);
+				}
+			}
+		})();
+	}, [sf]);
 
 	return (
-		<SuperFluidContext.Provider value={{ sf: sf, defaultTokenList }}>
+		<SuperFluidContext.Provider
+			value={{ sf: sf, defaultTokenList, userTokenList }}>
 			{children}
 		</SuperFluidContext.Provider>
 	);
