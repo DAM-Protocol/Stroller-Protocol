808343eb5e098befa8b2598f814fb386c3647950
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-02-07 05:44:46 +0530

    Update Landing.jsx

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,18 +1,44 @@
-import { Text, Heading, Flex } from '@chakra-ui/react';
+import { Text, Heading, Flex, Button, Divider, Box } from '@chakra-ui/react';
 import Page from '../components/layouts/Page';
+import { NavLink } from '../components/sections/Navbar/Links';
 
 const Landing = () => {
 	return (
 		<Page>
 			<Flex flexDirection='column' align='center' justify='center'>
-				<img src='./stroller-xxl.png' alt='X' style={{ width: '60px' }} />
-				<Heading as='h1' size='2xl'>
+				<img
+					src='./stroller-xxl.png'
+					alt='X'
+					style={{ width: '100px', margin: '2rem 0' }}
+				/>
+				<Heading as='h1' size='2xl' mb={10}>
 					Stroller Protocol
 				</Heading>
-				<Text>Unlock the potential of SuperTokens</Text>
-				<Text>
+				<Text fontWeight='bold' mb={3}>
+					Unlock the potential of SuperTokens
+				</Text>
+				<Text mb={10}>
 					Stroll is a protocol that provides automatic Top-Ups for Super Tokens
 				</Text>
+				<NavLink to='/dashboard'>
+					<Button colorScheme='green'>Go to Dashboard</Button>
+				</NavLink>
+				<Divider></Divider>
+				<Box height={'md'} width='xl'>
+					<iframe
+						title='Stroll Protocol - Deck'
+						loading='lazy'
+						style={{
+							width: '100%',
+							height: '100%',
+							border: 'none',
+							padding: 0,
+							margin: 0,
+						}}
+						src='https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAE3nEp1NNo&#x2F;view?embed'
+						allowfullscreen='allowfullscreen'
+						allow='fullscreen'></iframe>
+				</Box>
 			</Flex>
 		</Page>
 	);
