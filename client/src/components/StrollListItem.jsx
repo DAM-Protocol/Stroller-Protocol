db324d2bd198301415cc734fef48ed98f9be15a9
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2022-02-07 05:45:03 +0530

    Fetch relevant data

--- client/src/components/StrollListItem.jsx
+++ client/src/components/StrollListItem.jsx
@@ -17,33 +17,83 @@ import {
 import nFormatter from '../utils/numberFormatter';
 import { BiPlus, BiMinus } from 'react-icons/bi';
 import { useContext, useState, useEffect } from 'react';
-import { useMoralis } from 'react-moralis';
+import {
+	useMoralis,
+	useMoralisWeb3Api,
+	useWeb3ExecuteFunction,
+} from 'react-moralis';
 import { SuperFluidContext } from '../context/SuperFluidContext';
 import TableHeader from './Headers/TableHeader';
+import { AAVE_STROLL_OUT_ADDRESS } from '../utils/contractAddresses';
+import { supportedTokens } from '../utils/supportedTokens';
+import { REGISTRY_ABI } from '../abis/registry';
+import { ERC20_ABI } from '../abis/erc20';
 
-const StrollListItem = ({ totalAllowance = 0, isLoading }) => {
+const StrollListItem = ({ tokenData, totalAllowance = 0, isLoading }) => {
 	const { sf } = useContext(SuperFluidContext);
 	const { user, web3, Moralis } = useMoralis();
+
 	const [coin, setCoin] = useState({});
 	const [balance, setBalance] = useState(0);
+	const [flow, setFlow] = useState(0);
+
 	const mainBg = useColorModeValue('gray.100', 'gray.700');
 	const tableBg = useColorModeValue('blackAlpha.50', 'blackAlpha.50');
 
+	const { data: allowanceData, fetch: fetchAllowance } = useWeb3ExecuteFunction(
+		{
+			abi: ERC20_ABI,
+			contractAddress: supportedTokens[tokenData?.underlyingAddress]?.aToken,
+			functionName: 'allowance',
+			params: {
+				owner: user.get('ethAddress'),
+				spender: AAVE_STROLL_OUT_ADDRESS,
+			},
+		}
+	);
+	const { data: aaveBalance, fetch: fetchAaveBalance } = useWeb3ExecuteFunction(
+		{
+			abi: ERC20_ABI,
+			contractAddress: supportedTokens[tokenData?.underlyingAddress]?.aToken,
+			functionName: 'balanceOf',
+			params: {
+				owner: user.get('ethAddress'),
+			},
+		}
+	);
+	// console.log(allowanceData, aaveBalance);
+
 	useEffect(() => {
-		if (sf)
+		if (sf && tokenData)
 			(async () => {
-				const c = await sf.loadSuperToken(
-					'0x3a27ff22eef2db03e91613ca4ba37e21ee21458a'
-				);
+				const c = await sf.loadSuperToken(tokenData.id);
 				setCoin(c);
 				const balance = await c.realtimeBalanceOf({
 					account: user.get('ethAddress'),
 					providerOrSigner: web3.getSigner(user.get('ethAddress')),
 				});
-
+				const flow = await sf.cfaV1.getNetFlow({
+					superToken: tokenData.id,
+					account: user.get('ethAddress'),
+					providerOrSigner: web3.getSigner(user.get('ethAddress')),
+				});
+				setFlow(flow);
 				setBalance(balance.availableBalance);
+				await fetchAllowance({
+					params: {
+						owner: user.get('ethAddress'),
+						spender: AAVE_STROLL_OUT_ADDRESS,
+					},
+				});
+				console.log(
+					await fetchAaveBalance({
+						params: {
+							owner: user.get('ethAddress'),
+						},
+					})
+				);
 			})();
-	}, [sf, user, web3]);
+	}, [sf, tokenData, user, web3]);
 
 	return (
 		<AccordionItem>
@@ -71,12 +121,14 @@ const StrollListItem = ({ totalAllowance = 0, isLoading }) => {
 						<>
 							<HStack flex={3} align='center' justify='center'>
 								<Badge variant='outline' colorScheme='green' borderRadius='sm'>
-									USDC
+									{tokenData?.symbol}
 								</Badge>
 							</HStack>
-							<Text flex={6}>USDCx</Text>
-							<Text flex={8}>1</Text>
-							<Text flex={8}>{nFormatter(totalAllowance)}</Text>
+							<Text flex={6}>{tokenData?.name}</Text>
+							<Text flex={8}>{flow}</Text>
+							<Text flex={8}>
+								{nFormatter(Moralis.Units.FromWei(totalAllowance))}
+							</Text>
 							<Text flex={8}>
 								{nFormatter(Moralis.Units.FromWei(balance), 5)}
 							</Text>
@@ -93,10 +145,16 @@ const StrollListItem = ({ totalAllowance = 0, isLoading }) => {
 							<TableRowSkeleton />
 						) : (
 							<Tr>
-								<Td textAlign='center'>USDC</Td>
+								<Td textAlign='center'>amUSDT</Td>
 								<Td textAlign='center'>2/2/22</Td>
-								<Td textAlign='center'>10K</Td>
-								<Td textAlign='center'>100</Td>
+								{/* balance */}
+								<Td textAlign='center'>
+									{nFormatter(Moralis.Units.FromWei(balance), 3)}
+								</Td>
+								{/* allowance */}
+								<Td textAlign='center'>
+									{nFormatter(Moralis.Units.FromWei(3764873647836), 3)}
+								</Td>
 								<Td textAlign='center'>
 									<Button mr={3} colorScheme='red' size='sm' variant='outline'>
 										<BiMinus />
