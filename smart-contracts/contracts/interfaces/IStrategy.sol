18730d8044912ea1ac2b2a2c6f0cafa99f358d88
rashtrakoff <chinmay.vemuri@outlook.com>
2022-03-09 12:46:45 +0530

    Modified strategy interface and ERC20StrollOut contract along with its test file.

--- smart-contracts/contracts/interfaces/IStrategy.sol
+++ smart-contracts/contracts/interfaces/IStrategy.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.4;
 import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
 interface IStrategy {
-    event TopUp(address _user, address _superToken, uint256 _amount);
+    event TopUp(address _user, address _superToken, uint256 _superTokenAmount);
 
-    function topUp(address _user, address _lpToken, ISuperToken _superToken, uint256 _amount) external;
+    function topUp(address _user, ISuperToken _superToken, uint256 _superTokenAmount) external;
 
     function isSupportedSuperToken(ISuperToken _superToken) external view returns(bool);
 }
