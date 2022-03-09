5fb71464a4dad3ad357a3c9aba7c33cd677daca6
Harsh Prakash <prakashharsh32@gmail.com>
2022-03-09 11:54:34 +0530

    Added time buffer, todo: time buffer for gelato

--- smart-contracts/contracts/StrollManager.sol
+++ smart-contracts/contracts/StrollManager.sol
@@ -1,12 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+// import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./interfaces/IStrategy.sol";
-// import "./common/StrollHelper.sol";
-// import "./interfaces/IStrollResolver.sol";
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/IStrategy.sol";
+import {OpsReady, IOps} from "./keepers/OpsReady.sol";
 import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 
 interface IERC20Mod is IERC20 {
@@ -14,29 +13,24 @@ interface IERC20Mod is IERC20 {
 }
 
 // solhint-disable not-rely-on-time
-contract Registry is Ownable {
-    // using StrollHelper for ISuperToken;
-
-    struct Strategy {
-        IStrategy strategy;
-        address liquidityToken;
-    }
-
+contract Registry is Ownable, OpsReady {
     // Modify this struct later to support multiple strategies in the future
     struct TopUp {
         address user;
         ISuperToken superToken;
-        Strategy strategy;
+        IStrategy strategy;
+        address liquidityToken;
         uint64 time;
         uint64 lowerLimit;
         uint64 upperLimit;
     }
 
+    uint64 public minLower;
+    uint64 public minUpper;
     mapping(bytes32 => TopUp) private topUps; // user => superToken => uint
 
-    // IStrollResolver private immutable strollResolver;
-
     // For testnet deployment
+    // solhint-disable-next-line
     IConstantFlowAgreementV1 public immutable CFA_V1;
     // IConstantFlowAgreementV1(0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873);
 
@@ -44,24 +38,32 @@ contract Registry is Ownable {
         bytes32 indexed id,
         address indexed user,
         address indexed superToken,
-        address liquidityToken,
         address strategy,
-        uint256 time
+        address liquidityToken,
+        uint256 time,
+        uint256 lowerLimit,
+        uint256 upperLimit
     );
 
     event TopUpDeleted(
         bytes32 indexed id,
         address indexed user,
         address indexed superToken,
-        address liquidityToken,
         address strategy,
-        uint256 time
+        address liquidityToken
     );
 
     event PerformedTopUp(bytes32 indexed id);
 
-    constructor(address _icfa) {
+    constructor(
+        address _icfa,
+        address _ops,
+        uint64 _minLower,
+        uint64 _minUpper
+    ) OpsReady(_ops) {
         CFA_V1 = IConstantFlowAgreementV1(_icfa);
+        minLower = _minLower;
+        minUpper = _minUpper;
     }
 
     function getTopUpIndex(
@@ -94,24 +96,34 @@ contract Registry is Ownable {
 
         if (topUps[index].time == 0) {
             // create new topUp
-            TopUp memory topup = TopUp(
+            TopUp memory topUp = TopUp(
                 msg.sender,
                 ISuperToken(_superToken),
-                Strategy(IStrategy(_strategy), _liquidityToken),
+                IStrategy(_strategy),
+                _liquidityToken,
                 _time,
                 _lowerLimit,
                 _upperLimit
             );
-            topUps[index] = topup;
+            topUps[index] = topUp;
         }
+
+        IOps(ops).createTask(
+            address(this),
+            this.gelatoPerformTopUp.selector,
+            address(this),
+            abi.encodeWithSelector(this.gelatoCheckTopUp.selector, index)
+        );
 
         emit TopUpCreated(
             index,
             msg.sender,
             _superToken,
-            _liquidityToken,
             _strategy,
-            _time
+            _liquidityToken,
+            _time,
+            _lowerLimit,
+            _upperLimit
         );
     }
 
@@ -125,7 +137,8 @@ contract Registry is Ownable {
         returns (
             address,
             ISuperToken,
-            Strategy memory,
+            IStrategy,
+            address,
             uint64,
             uint64,
             uint64
@@ -141,21 +154,23 @@ contract Registry is Ownable {
         returns (
             address,
             ISuperToken,
-            Strategy memory,
+            IStrategy,
+            address,
             uint64,
             uint64,
             uint64
         )
     {
-        TopUp memory topup = topUps[_index];
+        TopUp memory topUp = topUps[_index];
 
         return (
-            topup.user,
-            topup.superToken,
-            topup.strategy,
-            topup.time,
-            topup.lowerLimit,
-            topup.upperLimit
+            topUp.user,
+            topUp.superToken,
+            topUp.strategy,
+            topUp.liquidityToken,
+            topUp.time,
+            topUp.lowerLimit,
+            topUp.upperLimit
         );
     }
 
@@ -163,80 +178,103 @@ contract Registry is Ownable {
         address _user,
         address _superToken,
         address _liquidityToken
-    ) public view returns (bool, uint256) {
+    ) public view returns (uint256) {
         bytes32 index = getTopUpIndex(_user, _superToken, _liquidityToken);
         return checkTopUp(index);
     }
 
-    function checkTopUp(bytes32 _index) public view returns (bool, uint256) {
-        TopUp memory topup = topUps[_index];
-        if (topup.time <= block.timestamp) {
-            return (false, 0);
-        }
+    function checkTopUp(bytes32 _index) public view returns (uint256) {
+        TopUp memory topUp = topUps[_index];
 
         if (
-            topup.user == address(0) || // Task exists and has a valid user
-            topup.time > block.timestamp || // Task exists and current time is before task end time
-            IERC20(topup.strategy.liquidityToken).allowance(
-                topup.user,
-                address(topup.strategy.strategy) // contract is allowed to spend
+            topUp.user == address(0) || // Task exists and has a valid user
+            topUp.time > block.timestamp || // Task exists and current time is before task end time
+            IERC20(topUp.liquidityToken).allowance(
+                topUp.user,
+                address(topUp.strategy) // contract is allowed to spend
             ) ==
             0 ||
-            IERC20(topup.strategy.liquidityToken).balanceOf(topup.user) == 0 // check user balance
-        ) return (false, 0);
+            IERC20(topUp.liquidityToken).balanceOf(topUp.user) == 0 // check user balance
+        ) return 0;
 
-        int96 flowRate = CFA_V1.getNetFlow(topup.superToken, topup.user);
+        int96 flowRate = CFA_V1.getNetFlow(topUp.superToken, topUp.user);
 
         if (flowRate < 0) {
-            uint256 superBalance = topup.superToken.balanceOf(topup.user);
+            uint256 superBalance = topUp.superToken.balanceOf(topUp.user);
             uint256 positiveFlowRate = uint256(uint96(-1 * flowRate));
 
-            if (superBalance <= (positiveFlowRate * topup.lowerLimit)) {
-                uint256 topUpAmount = positiveFlowRate * topup.upperLimit;
-                return (
-                    true,
+            if (superBalance <= (positiveFlowRate * topUp.lowerLimit)) {
+                uint256 topUpAmount = positiveFlowRate * topUp.upperLimit;
+                return
                     topUpAmount /
-                        10 **
-                            (18 -
-                                IERC20Mod(topup.superToken.getUnderlyingToken())
-                                    .decimals())
-                );
+                    10 **
+                        (18 -
+                            IERC20Mod(topUp.superToken.getUnderlyingToken())
+                                .decimals());
             }
         }
 
-        return (false, 0);
+        return 0;
+    }
+
+    function performTopUp(
+        address _user,
+        address _superToken,
+        address _liquidityToken
+    ) external {
+        bytes32 index = getTopUpIndex(_user, _superToken, _liquidityToken);
+        performTopUp(index);
     }
 
     function performTopUp(bytes32 _index) public {
-        TopUp memory topup = topUps[_index];
-        (bool check, uint256 topUpAmount) = checkTopUp(_index);
-        require(check, "TopUp check failed");
-        topup.strategy.strategy.topUp(
-            topup.user,
-            topup.strategy.liquidityToken,
-            ISuperToken(topup.superToken),
+        TopUp memory topUp = topUps[_index];
+        uint256 topUpAmount = checkTopUp(_index);
+        require(topUpAmount > 0, "TopUp check failed");
+        topUp.strategy.topUp(
+            topUp.user,
+            topUp.liquidityToken,
+            ISuperToken(topUp.superToken),
             topUpAmount
         );
         emit PerformedTopUp(_index);
     }
 
+    function gelatoCheckTopUp(bytes32 _index)
+        public
+        view
+        returns (bool canExec, bytes memory execPayload)
+    {
+        // TopUp memory topup = topUps[_index];
+        uint256 topUpAmount = checkTopUp(_index);
+
+        canExec = topUpAmount > 0;
+        execPayload = abi.encodeWithSelector(
+            this.gelatoPerformTopUp.selector,
+            _index
+        );
+    }
+
+    function gelatoPerformTopUp(bytes32 _index) public onlyOps {
+        performTopUp(_index); //perform TopUp
+
+        (uint256 fee, address feeToken) = IOps(ops).getFeeDetails(); //pay fee to gelato
+        _transfer(fee, feeToken);
+    }
+
     function deleteTopUp(bytes32 _index) public {
-        TopUp memory topup = topUps[_index];
-        require(topup.time > 0, "TopUp does not exist");
+        TopUp memory topUp = topUps[_index];
+        require(topUp.time > 0, "TopUp does not exist");
         require(
-            topup.user == msg.sender || topup.time < block.timestamp,
+            topUp.user == msg.sender || topUp.time < block.timestamp,
             "Can't delete TopUp"
         );
-        topUps[_index].time = 0;
-
-        topUps[_index].user = address(0);
+        delete topUps[_index];
         emit TopUpDeleted(
             _index,
-            topup.user,
-            address(topup.superToken),
-            topup.strategy.liquidityToken,
-            address(topup.strategy.strategy),
-            topup.time
+            topUp.user,
+            address(topUp.superToken),
+            address(topUp.strategy),
+            topUp.liquidityToken
         );
     }
 
