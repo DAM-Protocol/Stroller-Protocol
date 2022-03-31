93c67a9624cf3a61614aef3042eb5f3a1f6f6e14
ngmachado <ngsmo@iscte-iul.pt>
2022-03-31 15:55:19 +0200

    add createTopUp to interface (extending tests)

--- smart-contracts/contracts/interfaces/IStrollManager.sol
+++ smart-contracts/contracts/interfaces/IStrollManager.sol
@@ -7,4 +7,13 @@ interface IStrollManager {
     function performTopUp(uint256 _index) external;
 
     function getTotalTopUps() external view returns (uint256);
+
+    function createTopUp(
+        address _superToken,
+        address _strategy,
+        address _liquidityToken,
+        uint64 _expiry,
+        uint64 _lowerLimit,
+        uint64 _upperLimit
+    ) external;
 }
