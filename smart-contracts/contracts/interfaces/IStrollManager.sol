ee12baf736d7e3cd7f49e63e2e96c6ea5bfaea9b
Harsh Prakash <prakashharsh32@gmail.com>
2022-05-29 20:57:28 +0530

    Added natspec to inteface

--- smart-contracts/contracts/interfaces/IStrollManager.sol
+++ smart-contracts/contracts/interfaces/IStrollManager.sol
@@ -15,13 +15,7 @@ interface IStrollManager {
         uint256 lowerLimit,
         uint256 upperLimit
     );
-    event TopUpDeleted(
-        bytes32 indexed id,
-        address indexed user,
-        address indexed superToken,
-        address strategy,
-        address liquidityToken
-    );
+    event TopUpDeleted(bytes32 indexed id, address indexed user, address indexed superToken, address strategy, address liquidityToken);
     event PerformedTopUp(bytes32 indexed id, uint256 topUpAmount);
     event AddedApprovedStrategy(address indexed strategy);
     event RemovedApprovedStrategy(address indexed strategy);
@@ -53,6 +47,27 @@ interface IStrollManager {
         uint64 upperLimit;
     }
 
+    /**
+     * @notice Adds a strategy to the list of approved strategies.
+     * @param _strategy The address of strategy contract to add.
+     */
+    function addApprovedStrategy(address _strategy) external;
+
+    /**
+     * @notice Removes a strategy from the list of approved strategies.
+     * @param _strategy The address of strategy contract to remove.
+     */
+    function removeApprovedStrategy(address _strategy) external;
+
+    /**
+     *  @notice Creates a new top up task.
+     *  @param _superToken The supertoken to monitor/top up.
+     *  @param _strategy The strategy to use for top up.
+     *  @param _liquidityToken The token used to convert to _superToken.
+     *  @param _expiry Timestamp after which the top up is considered invalid.
+     *  @param _lowerLimit Triggers top up if stream can't be continued for this amount of seconds.
+     *  @param _upperLimit Increase supertoken balance to continue stream for this amount of seconds.
+     */
     function createTopUp(
         address _superToken,
         address _strategy,
@@ -62,53 +77,113 @@ interface IStrollManager {
         uint64 _upperLimit
     ) external;
 
-    function performTopUp(
+    /**
+     * @notice Gets the index of a top up.
+     * @param _user The creator of top up.
+     * @param _superToken The supertoken which is being monitored/top up.
+     * @param _liquidityToken The token used to convert to _superToken.
+     * @return The index of the top up.
+     */
+    function getTopUpIndex(
         address _user,
         address _superToken,
         address _liquidityToken
-    ) external;
+    ) external pure returns (bytes32);
 
-    function deleteTopUp(
+    /**
+     * @notice Gets a top up by index.
+     * @param _index Index of top up.
+     * @return The top up.
+     */
+    function getTopUpByIndex(bytes32 _index) external view returns (TopUp memory);
+
+    /**
+     * @notice Gets a top up by index.
+     * @param _user The creator of top up.
+     * @param _superToken The supertoken which is being monitored/top up.
+     * @param _liquidityToken The token used to convert to _superToken.
+     * @return The top up.
+     */
+    function getTopUp(
         address _user,
         address _superToken,
         address _liquidityToken
-    ) external;
+    ) external view returns (TopUp memory);
 
-    function performTopUpByIndex(bytes32 _index) external;
+    /**
+     * @notice Checks if a top up is required by index.
+     * @param _index Index of top up.
+     * @return _amount The amount of supertoken to top up.
+     */
+    function checkTopUpByIndex(bytes32 _index) external view returns (uint256 _amount);
 
-    function deleteTopUpByIndex(bytes32 _index) external;
-
-    function deleteBatch(bytes32[] calldata _indices) external;
-
-    function addApprovedStrategy(address _strategy) external;
-
-    function removeApprovedStrategy(address _strategy) external;
-
-    function minLower() external view returns (uint64);
-
-    function minUpper() external view returns (uint64);
-
-    function approvedStrategies(address _strategy) external view returns (bool);
-
+    /**
+     * @notice Checks if a top up is required.
+     * @param _user The creator of top up.
+     * @param _superToken The supertoken which is being monitored/top up.
+     * @param _liquidityToken The token used to convert to _superToken.
+     * @return _amount The amount of supertoken to top up.
+     */
     function checkTopUp(
         address _user,
         address _superToken,
         address _liquidityToken
     ) external view returns (uint256);
 
-    function getTopUpByIndex(bytes32 _index)
-        external
-        view
-        returns (TopUp memory);
+    /**
+     * @notice Performs a top up by index.
+     * @param _index Index of top up.
+     */
+    function performTopUpByIndex(bytes32 _index) external;
 
-    function checkTopUpByIndex(bytes32 _index)
-        external
-        view
-        returns (uint256 _amount);
-
-    function getTopUpIndex(
+    /**
+     * @notice Performs a top up.
+     * @param _user The user to top up.
+     * @param _superToken The supertoken to monitor/top up.
+     * @param _liquidityToken The token used to convert to _superToken.
+     */
+    function performTopUp(
         address _user,
         address _superToken,
         address _liquidityToken
-    ) external pure returns (bytes32);
+    ) external;
+
+    /**
+     * @notice Deletes a top up by index.
+     * @param _index Index of top up.
+     */
+    function deleteTopUpByIndex(bytes32 _index) external;
+
+    /** @dev IStrollManager.deleteTopUp implementation.
+     * @notice Deletes a top up.
+     * @param _user The creator of top up.
+     * @param _superToken The supertoken which is being monitored/top up.
+     * @param _liquidityToken The token used to convert to _superToken.
+     */
+    function deleteTopUp(
+        address _user,
+        address _superToken,
+        address _liquidityToken
+    ) external;
+
+    /**  @dev IStrollManager.deleteBatch implementation.
+     * @notice Deletes a batch of top ups.
+     * @param _indices Array of indices of top ups to delete.
+     */
+    function deleteBatch(bytes32[] calldata _indices) external;
+
+    /**
+     * @notice Gets the minimum time for _lowerLimit
+     */
+    function minLower() external view returns (uint64);
+
+    /**
+     * @notice Gets the minimum time for _upperLimit
+     */
+    function minUpper() external view returns (uint64);
+
+    /**
+     * @notice Gets the list of approved strategies.
+     */
+    function approvedStrategies(address _strategy) external view returns (bool);
 }
