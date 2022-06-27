36d758f62c2381f5bb0e6cc53229e7eea9db9f6c
rashtrakoff <chinmay.vemuri@outlook.com>
2022-06-27 17:49:51 +0530

    Changed SPDX license from 'Unlicense' to 'AGPL-3.0-Only'

--- smart-contracts/contracts/strategies/AaveV2StrollOut.sol
+++ smart-contracts/contracts/strategies/AaveV2StrollOut.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.13;
 
 import { ILendingPoolAddressesProvider, ILendingPool, IProtocolDataProvider } from "../interfaces/IAaveV2.sol";
